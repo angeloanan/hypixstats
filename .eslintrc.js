427f2048c0569db92916f89484578acd2153bcea
Christopher Angelo <angelo@angeloanan.xyz>
2022-07-16 14:59:32 +0700

    Initial commit
    Template from react-native-template-typescript

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native-community',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+      },
+    },
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
